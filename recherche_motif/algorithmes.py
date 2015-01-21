53c17df4ab44510b94dff4a53d6a41bae8957549
JulienRouse <julien.rouse@gmail.com>
2015-01-21 11:16:45 +0100

    ajout de quelques performances readme, changements mineurs ailleurs

--- recherche_motif/algorithmes.py
+++ recherche_motif/algorithmes.py
@@ -33,7 +33,7 @@ def brute_force(motif,chaine_adn):
 
     '''
     occ = 0 
-    len_chaine_adn = len(chaine_adn)
+    len_chaine_adn = len(chaine_adn) 
     len_motif = len(motif)
     for c1 in xrange(0,len_chaine_adn-len_motif+1):  
         for c2 in xrange(0,len_motif):
@@ -50,26 +50,26 @@ def brute_force(motif,chaine_adn):
 
 def hachage_rabin_karp(motif):
 	
-	#effectue un hachage defini par l'algorithme de rabin-karp sur un motif passe en parametre
+    #effectue un hachage defini par l'algorithme de rabin-karp sur un motif passe en parametre
 	
-	base_azotee = { 'A':1 , 'C':2 , 'G':3 , 'T':4 }
-	return sum([base_azotee[c] for c in motif])	
+    base_azotee = { 'A':1 , 'C':2 , 'G':3 , 'T':4 }
+    return sum([base_azotee[c] for c in motif])	
 	
 def rabin_karp(motif,chaine_adn):
 	
-	#recherche de motif avec l'algorithme de rabin_karp
-	#@param string,string le motif et la chaine dans laquelle on cherche ce motif
+    #recherche de motif avec l'algorithme de rabin_karp
+    #@param string,string le motif et la chaine dans laquelle on cherche ce motif
 	
-	occ = 0
-	hachage_motif = hachage_rabin_karp(motif)
-	chaine_hachage = range(0,len(chaine_adn)+1)
+    occ = 0
+    hachage_motif = hachage_rabin_karp(motif)
+    chaine_hachage = range(0,len(chaine_adn)+1)
 	
-	for i in xrange(0,len(chaine_adn)-len(motif)+1):
-		chaine_hachage[i] = hachage_rabin_karp(chaine_adn[i:len(motif)])
-		if (chaine_hachage[i]==hachage_motif):
-			occ+=1
+    for i in xrange(0,len(chaine_adn)-len(motif)+1):
+        chaine_hachage[i] = hachage_rabin_karp(chaine_adn[i:len(motif)])
+        if (chaine_hachage[i]==hachage_motif):
+            occ+=1
 	
-	return occ,chaine_hachage 
+    return occ,chaine_hachage 
 
 def cherche_generique(motif,chaine_adn,func=brute_force):
     '''
@@ -85,46 +85,64 @@ def cherche_generique(motif,chaine_adn,func=brute_force):
     return occ_motif + occ_inv + occ_comp + occ_comp_inv
     
 def boyer_moore(motif, chaine_adn) :
-	dico = apprentissage_boyer_moore(motif)
-	cpt = len(motif)-1
-	occ = 0
-	indice_occ = []
-	while (cpt < len(chaine_adn)) :
-		print cpt
-		boolean, cpt = compare_boyer_moore(motif, chaine_adn, cpt) 
-		if (boolean) :
-			print cpt
-			occ += 1
-			indice_occ.append(cpt)
-			cpt += len(motif)
-		else :
-			if (chaine_adn[cpt] in dico) :
-				cpt += dico[chaine_adn[cpt]]
-			else :
-				cpt += len(motif)
-	return occ,indice_occ
+    '''
+    Implémentation de l'algorithme de Boyer-Moore
+    
+    :param motif: Le motif à trouver dans la chaine_adn
+    :param chaine_adn: Chaine de caractère representant un séquence d'AA
+    :type motif: string
+    :type chaine_adn: string
+    :return: nombre d'occurence du motif dans la chaine,
+             les indices de ces occurences dans la chaine
+    :rtype: int,[int]
+
+    :Exemple:
+    
+    >>> boyer_moore("GATACA","GATACAACACATACAGATACATATAG")
+    2
+    
+    .. seealso:: brute_force()
+    .. note:: Boyer-Moore est plus performant sur des long motifs et/ou sur des 
+              alphabets étendus en théorie
+    '''
+    dico = apprentissage_boyer_moore(motif)
+    cpt = len(motif)-1
+    occ = 0
+    indice_occ = []
+    while (cpt < len(chaine_adn)) :
+        boolean, cpt = compare_boyer_moore(motif, chaine_adn, cpt) 
+        if (boolean) :
+            occ += 1
+            indice_occ.append(cpt)
+            cpt += len(motif)
+        else :
+            if (chaine_adn[cpt] in dico) :
+                    cpt += dico[chaine_adn[cpt]]
+            else :
+                cpt += len(motif)
+    return occ,indice_occ
 
 
 def apprentissage_boyer_moore(motif) : 
-	list_key = []
-	list_value = []
-	fitom = util.inverse(motif) 
-	for m in fitom[1:]: # tester avec ou sans slice operator
-		if (m not in list_key and m != fitom[0]):
-			list_key.append(m)
-			list_value.append(fitom.index(m))
-	return dict(zip(list_key, list_value))		
+    list_key = []
+    list_value = []
+    fitom = util.inverse(motif) 
+    for m in fitom[1:]: # tester avec ou sans slice operator
+        if (m not in list_key and m != fitom[0]):
+            list_key.append(m)
+            list_value.append(fitom.index(m))
+    return dict(zip(list_key, list_value))		
 
 def compare_boyer_moore(motif, chaine_adn, cpt) :
-	i = 0
-	size = len(motif)-1
-	while (i <= size) :
-		print motif[size-i], chaine_adn[cpt-i]
-		if (motif[size-i] == chaine_adn[cpt-i]) :
-			i += 1
-		else :
-			return False,cpt-i
-	return True,cpt-i+1
+    i = 0
+    size = len(motif)-1
+    while (i <= size) :
+        print motif[size-i], chaine_adn[cpt-i]
+        if (motif[size-i] == chaine_adn[cpt-i]) :
+	       	i += 1
+        else :
+            return False,cpt-i
+    return True,cpt-i+1
 
 
 
