56258ec76c93b0c9f836d7025e43e6ebbb7a9551
JulienRouse <julien.rouse@gmail.com>
2015-01-21 11:46:39 +0100

    debut de gestion d'argument pour le main

--- recherche_motif/main.py
+++ recherche_motif/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys
+import sys,getopt
 
 import algorithmes as algo
 import util 
@@ -10,8 +10,7 @@ adn = "GATACATTCATAGCTATGTGATACAGTATC"
 m = 'GATACA'   
 
 
-if __name__ == "__main__" :
-    
+'''
     narg = len(sys.argv) #nombre d'argument sur la ligne de commande
     if (narg<2): #0arg supplementaire
         adn = util.open_fasta('test.fasta')
@@ -22,3 +21,30 @@ if __name__ == "__main__" :
         adn = util.open_fasta(filename)
         occ = algo.cherche_generique(m,adn)
         print occ
+'''
+def main(argv):
+    algos = ["boyer_moore","brute_force"]
+    inputfile = ''
+    taille = 1000000
+    outputfile = ''
+   
+    try:
+        opts, args = getopt.getopt(argv,"lhi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print sys.argv[0]+' -i <inputfile> -o <outputfile>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print sys.argv[0]+' -l : Affiche la liste des algos utilisables'  
+            print sys.argv[0]+' -i <inputfile> -o <outputfile> -a <algorithme>'
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+        elif opt in ("-l", "--listeAlgo"):
+            for a in algos:
+                print a
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
