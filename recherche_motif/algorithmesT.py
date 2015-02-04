c510f4b026c095e3ee728878c3595f14dc1f79ba
JulienRouse <julien.rouse@gmail.com>
2015-02-04 17:25:35 +0100

    ajout de commentaires

--- recherche_motif/algorithmesT.py
+++ recherche_motif/algorithmesT.py
@@ -4,11 +4,18 @@
 from util import *
 import algorithmes as algo
 from multiprocessing.pool import Pool
-
+from joblib import Parallel, delayed
 
  
 
 def unwrap(a_b):
+    '''
+    unpack args
+    
+    :Example:
+    
+    >>> unwrap([
+    '''
     return algo.brute_force(*a_b)
 
 def cherche_generiqueT(motif,chaine_adn,func=algo.brute_force,comp_dic=complement_dic_adn):
@@ -28,21 +35,61 @@ def cherche_generiqueT(motif,chaine_adn,func=algo.brute_force,comp_dic=complemen
     c = chaine_adn
     #version ac map
     res= p.map(unwrap, [(motif,c),(inv,c),(comp,c),(comp_inv,c)])
-    #p.close
-    #p.join()
+    p.close
+    p.join()
     
     #version ac apply_async
     #res_intermediaire = [p.apply_async(func,args=x) for x in [(motif,c),(inv,c),(comp,c),(comp_inv,c)]]
     #p.close()
     #p.join()
-    res = [p.get() for p in res_intermediaire]
+    #res = [p.get() for p in res_intermediaire]
     occ = res[0][0] + res[1][0] + res[2][0] + res[3][0]
     indice_occ =  res[0][1] + res[1][1] + res[2][1] + res[3][1]
     return occ, sorted(indice_occ)
     
 
 
-#print cherche_generiqueT('GAGA','GATATAGATAGAGAGGAATATGATAGATAGTAGAGACCCC')
+
+def cherche_generiqueP(motif,chaine_adn,func=algo.brute_force,comp_dic=complement_dic_adn):
+    '''
+    Recherche un motif, son complément, son inverse et son complément_inverse et additione leurs occurences en
+    utilisant plusieurs processus pour essayer d'améliorer les performances de recherche_generique().
+    
+    :param motif: Le motif dont on cherche l'indice et le nombre d'occurence.
+    :param chaine_adn: La chaine dans laquelle on fait la recherche.
+    :param func: L'algorithme de recherche de motif à utiliser. Par défaut, utilise brute_force défini dans algorithmes.py
+    :param comp_dic: Le dictionnaire définissant les complémentaires en fonction des AA présent dans la chaine_adn. Par défaut, utilise complement_dic_adn défini dans util
+    :type motif: string
+    :type chaine_adn: string
+    :type func: func(string,string)
+    :type comp_dic: dic(char,char)
+    :return: Un tuple contenant :
+             -La somme des occurence d'un motif, de son inverse, de son complément, et de son complément inverse.
+             -La liste des indices des occurences du motif, de son inverse, de son complément, et de son complément inverse.
+    :rtype: (int,[int])
+    
+    :Exemples:
+    
+    TODO
+    
+
+    ..seealso::cherche_generique(), cherche_generiqueT()
+
+
+    '''
+
+    indice_occ = []
+    inv = inverse(motif)
+    comp = complement(motif,comp_dic)
+    comp_inv = complement_inverse(motif,comp_dic)
+    c = chaine_adn
+    
+    res = Parallel(n_jobs=4)(delayed(func)(m,c) for m in [motif,inv,comp,comp_inv])
+    occ = res[0][0] + res[1][0] + res[2][0] + res[3][0]
+    indice_occ =  res[0][1] + res[1][1] + res[2][1] + res[3][1]
+    return occ, sorted(indice_occ)
+    
+    
 
 
 
--- recherche_motif/tmp_juR.py
+++ recherche_motif/tmp_juR.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
 import algorithmesT as algoT
 import util
 import algorithmes as algo
@@ -20,7 +24,7 @@ def cherche_mot_taille_N(chaine_adn,n,func=algo.brute_force,comp=util.complement
         motif = chaine_adn[i:i+n]
         if (motif not in connu):
             occ,_ = algoT.cherche_generiqueP(motif,chaine_adn,algo_recherche,comp)
-            occ,_ = func(motif,chaine_adn)
+           # occ,_ = func(motif,chaine_adn)
             connu[motif] = occ
             #connu[(util.inverse(motif))] = occ
             #connu[(util.complement(motif,comp))] = occ
@@ -61,19 +65,46 @@ def cherche_mot_taille_NT(chaine_adn,n,func=algo.brute_force,comp=util.complemen
 
 
 def cherche_mot_taille_N_essai(chaine_adn,n):
-    len_chaine_adn = len(chaine_adn)
-    connu = {}
+    '''
+    Calcule le nombre d'occurence de tous les mots de taille n présent dans chaine_adn.
+    
+    :param chaine_adn: La chaine dans laquelle on recherche les mots.
+    :param n: La taille des mots que l'on souhaite chercher.
+    :type chaine_adn: string
+    :type n: int
+    :return: Une liste de tuple contenant:
+             -Le mot trouvé
+             -Son nombre d'occurence.
+    :rtype: [(string,int)]
+    
+    :Exemple:
+    
+    >>> sorted(cherche_mot_taille_N_essai('AGCTCCATC',2))
+    [('AG', 1), ('AT', 1), ('CA', 1), ('CC', 1), ('CT', 1), ('GC', 1), ('TC', 2)]
+   
+ 
+    ..seealso:: 
+    ..warning:: Le nombre de mot possible est (nombre_lettre_dans_lalphabet**n) qui est ici
+                4**n. Le nombre de  mot possible augmente de manière exponentielle.
+    ..performance:: d'après: https://wiki.python.org/moin/TimeComplexity
+                    Soit M la taille de chaine_adn, N la taille des mots que l'on cherche,
+                    P le nombre de mot partageant un hash dans le dictionnaire. 
+                    (ici P=4**N dans le pire des cas, 1 le plus souvent)
+                    O(1) + O(M-N) * (O(N) + (O(P)) + (O(4**N)) ?
+    '''
+    len_chaine_adn = len(chaine_adn) #O(1)
+    connu = {} 
     motif = ''
     
-    for i in xrange(0,len_chaine_adn-n+1):
-        motif = chaine_adn[i:i+n]
-        
-        if motif in connu:
-            connu[motif] += 1 
+    
+    for i in xrange(0,len_chaine_adn-n+1): #O(M-N)
+        motif = chaine_adn[i:i+n] #O(N) 
+            if motif in connu:
+            connu[motif] += 1  #O(1) en pratique, #O(P) dans le pire des cas.
         else:
-            connu[motif] = 1
-    #from operator import itemgetter
-    return [(a,connu[a]) for a in connu]#sorted(connu,key=itemgetter(0))]
+            connu[motif] = 1 #O(1) en pratique, #O(P) dans le pire des cas.
+    return [(a,connu[a]) for a in connu] #O(4**N) dans le pire des cas.
+
 
 def cherche_mot_taille_N_essai_counter(chaine_adn,n):
     from collections import Counter
@@ -95,11 +126,11 @@ if __name__ == '__main__':
         doctest.testmod()
     else:
 
-        f = util.open_fasta("test10millions.fasta")
-        #f = util.open_fasta("../data/chromosome13_NT_009952.14.fasta")
+        #f = util.open_fasta("test10millions.fasta")
+        f = util.open_fasta("../data/chromosome13_NT_009952.14.fasta")
         #l = cherche_mot_taille_N(f,int(sys.argv[1]),comp=util.complement_dic_arn)
         l2 = cherche_mot_taille_N_essai(f,int(sys.argv[1]))
-        #print l2
+        print l2
 
 
 
