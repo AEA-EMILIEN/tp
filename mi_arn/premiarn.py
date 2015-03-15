bb7e1448538bde9f50e27d8ece32fe5441ceb170
Emilie Allart <emilie.allart@etudiant.univ-lille1.fr>
2015-03-15 17:19:05 +0100

    implémentation de n_w

--- mi_arn/premiarn.py
+++ mi_arn/premiarn.py
@@ -1,9 +1,26 @@
 import numpy as np
 
-def Sub(x1,x2) :
-	if x1 == x2 :
-		return 2
+complement_dic_arn_2 = {'A':'U',
+    'U':('A','G'),
+    'G':('C','U'),
+    'C':'G'}
+	 
+complement_dic_adn = {'A':'T',
+    'T':'A',
+    'G':'C',
+    'C':'G'}
+
+def Sub(m1, m2,dic=complement_dic_adn) :
+	comp = dic[m1]
+	if len(comp)>1 :
+		for c in comp :
+			if c == m2 :
+				return 2
+	else :
+		if comp == m2 :
+			return 2
 	return -1
+
 
 def N_W(u,v) :
 	'''
@@ -14,10 +31,10 @@ def N_W(u,v) :
 	matx = np.zeros((len_u+1,len_v+1))
 	max_val = -1
 	max_ind = (-1,-1)
-	Del = -2
-	Ins = -2
-	for cpt_u in range(1,len_u) :
-		for cpt_v in range(1,len_v) :
+	Del = -1
+	Ins = -1
+	for cpt_u in range(1,len_u+1) :
+		for cpt_v in range(1,len_v+1) :
 			sub = matx[cpt_u-1,cpt_v-1] + Sub(u[cpt_u-1],v[cpt_v-1]) 
 			dele = matx[cpt_u-1,cpt_v] + Del
 			ins = matx[cpt_u,cpt_v-1] + Ins
